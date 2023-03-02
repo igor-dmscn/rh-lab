b52590497d3b1a6dfda83d1861fd771bac7e292b
Igor Damasceno <igor.damasceno@codeminer42.com>
2023-03-02 12:16:39 -0300

    test: add employee index tests

--- spec/requests/employees_spec.rb
+++ spec/requests/employees_spec.rb
@@ -3,7 +3,91 @@
 require 'rails_helper'
 
 RSpec.describe 'Employees' do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /employees' do
+    context 'with html format' do
+      context 'when there are no employees' do
+        it 'returns successful response' do
+          get employees_path
+
+          expect(response).to be_successful
+        end
+
+        it 'renders index html template' do
+          get employees_path
+
+          expect(response).to render_template(:index)
+        end
+        
+        it 'assigns empty employees' do
+          get employees_path
+          expect(assigns(:employees)).to eq([])
+        end
+      end
+
+      context 'when there are unemployed employees' do
+        it 'returns successful response' do
+          get employees_path
+
+          expect(response).to be_successful
+        end
+
+        it 'renders index html template' do
+          get employees_path
+
+          expect(response).to render_template(:index)
+        end
+        
+        it 'assigns empty employees' do
+          employee = Employee.create(name: 'Employee')
+
+          get employees_path
+
+          expect(assigns(:employees)).to eq([])
+        end
+      end
+
+      context 'when there are employed employees' do
+        it 'returns successful response' do
+          get employees_path
+
+          expect(response).to be_successful
+        end
+
+        it 'renders index html template' do
+          get employees_path
+
+          expect(response).to render_template(:index)
+        end
+        
+        it 'assigns empty employees' do
+          employee = Employee.create(name: 'Employee')
+          department = Department.create(name: 'Department', manager: employee)
+          position = Position.create(name: 'Position')
+          employment = Employment.create(employee: employee, department: department, position: position, salary: 1000, registration: '1', starts_on: Date.today)
+
+          get employees_path
+
+          expect(assigns(:employees)).to eq([employee])
+        end
+      end
+    end
+
+    context 'with json format' do
+      it 'returns successful response' do
+        get employees_path, headers: { 'Content-Type': 'application/json' }
+
+        expect(response).to be_successful
+      end
+
+      it 'renders index json template' do
+        get employees_path, headers: { 'Content-Type': 'application/json' }
+        expect(response).to render_template(:index)
+      end
+
+      it 'assigns employees' do
+        get employees_path
+        expect(assigns(:employees))
+      end
+    end
   end
 end
