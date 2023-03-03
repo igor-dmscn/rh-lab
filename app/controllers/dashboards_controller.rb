de1a9dcfcc86724e8bfa3e30da14f93ba1bc9d0b
Igor Damasceno <igor.damasceno@codeminer42.com>
2023-03-03 11:10:51 -0300

    feat: add show overbudget projects

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
+OVERBUDGET_PROJECT_ROWS = 5
 
 class DashboardsController < ApplicationController
   def show
+
     @average_position_salary = Employment.group(:position).average(:salary)
+    
+    # @overbudget_projects = Project.find_by_sql('SELECT p.name, (p.spent - p.budget) AS overbudget FROM projects p ORDER BY overbudget DESC LIMIT 5')
+    @overbudget_projects = Project.select('name, (spent - budget) AS overbudget').order(overbudget: :desc).limit(OVERBUDGET_PROJECT_ROWS)
   end
 end
