9ed9812e272cf52c6135abad869f91daf1a6cbbe
Igor Damasceno <igor.damasceno@codeminer42.com>
2023-03-03 12:50:41 -0300

    feat: add dashboard active employees count

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,8 +5,11 @@ class DashboardsController < ApplicationController
   def show
 
     @average_position_salary = Employment.group(:position).average(:salary)
-    
+
     # @overbudget_projects = Project.find_by_sql('SELECT p.name, (p.spent - p.budget) AS overbudget FROM projects p ORDER BY overbudget DESC LIMIT 5')
     @overbudget_projects = Project.select('name, (spent - budget) AS overbudget').order(overbudget: :desc).limit(OVERBUDGET_PROJECT_ROWS)
+
+    # @active_employees = Department.joins(:employments).where(employments: { ends_on: nil }).group(:id).pluck(:name, 'count(employments.employee_id)')
+    @active_employees = Department.active_employments.group(:id).pluck(:name, 'count(employments.employee_id)')
   end
 end
