0765e95a7a2bb4fcd3bc025daf86a496ae387b75
Igor Damasceno <igor.damasceno@codeminer42.com>
2023-03-03 10:51:48 -0300

    feat: position average salary

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class DashboardsController < ApplicationController
-  def show; end
+  def show
+    @average_position_salary = Employment.group(:position).average(:salary)
+  end
 end
